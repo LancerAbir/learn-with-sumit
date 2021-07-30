f96ac8f7f94883b6018a94e2b65b9ed0f3aa869c
lancer abir <you@example.com>
2021-07-30 12:33:17 +0600

    dad

--- react-js/lesson-3/src/Components/Clock.js
+++ react-js/lesson-3/src/Components/Clock.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 const Clock = ({ local }) => {
-    return (
-        <div>
-            <h2>
-                Clock Array Functional Components{" "}
-                {new Date().toLocaleString(local)}{" "}
-            </h2>
-        </div>
-    );
+   return (
+      <div>
+         <h2>
+            Clock Array Functional Components {new Date().toLocaleString(local)}{" "}
+         </h2>
+      </div>
+   );
 };
 
 export default Clock;
