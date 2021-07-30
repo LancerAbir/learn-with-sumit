13438a501aa8e22216ca7954b3e9b4516fa4b4cd
lancer abir <you@example.com>
2021-07-30 12:34:57 +0600

    some change

--- react-js/lesson-3/src/App.js
+++ react-js/lesson-3/src/App.js
@@ -3,13 +3,13 @@ import Clock from "./Components/Clock";
 import Test from "./Components/Test";
 
 function App() {
-    return (
-        <div>
-            <h2>Hello Lancer Abir </h2>
-            <Test />
-            <Clock local="bn-BD" />
-        </div>
-    );
+   return (
+      <div>
+         <h2>Hello Lancer Abir </h2>
+         <Test />
+         <Clock local="bn-BD" />
+      </div>
+   );
 }
 
 export default App;
