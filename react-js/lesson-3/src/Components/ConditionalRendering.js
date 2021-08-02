988b1453be578afc195d1f639261e186449863ce
Lancer Abir <anjan.o24@gmail.com>
2021-08-02 12:09:19 +0600

    lesson 9 done

--- react-js/lesson-3/src/Components/ConditionalRendering.js
+++ react-js/lesson-3/src/Components/ConditionalRendering.js
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
 
 export default class ConditionalRendering extends Component {
-   state = {
-      abir: "for chaity",
-      chaity: "for abir",
-   };
+    state = {
+        abir: "for chaity",
+        chaity: "for abir",
+    };
 
-   changeHandler = (e) => {
-      e.preventDefault();
-      this.setState({
-         abir: "for chaity",
-         chaity: "for abir",
-      });
-   };
-   render() {
-      const { abir, chaity } = this.state;
+    changeHandler = (e) => {
+        e.preventDefault();
+        this.setState({
+            abir: "for chaity",
+            chaity: "for abir",
+        });
+    };
+    render() {
+        const { abir, chaity } = this.state;
 
-      return (
-         <div>
-            <h2>Lesson 9 - Conditional Rendering</h2>
-         </div>
-      );
-   }
+        return (
+            <div>
+                <h2>Lesson 9 - Conditional Rendering</h2>
+                <ul>
+                    <li> if else (return এর বাইরে ব্যাবহার করতে হয়) </li>
+                    <li>
+                        {" "}
+                        ternary Operator (return এর ভিতরে ব্যাবহার করতে হয়){" "}
+                    </li>
+                </ul>
+            </div>
+        );
+    }
 }
